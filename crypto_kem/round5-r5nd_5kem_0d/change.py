1231e5d680995887a286d61b73cec6824187729f
lrubens <rubenslacouture@gmail.com>
2019-08-12 15:29:12 +0000

    adding prefix to an array name

--- crypto_kem/round5-r5nd_5kem_0d/change.py
+++ crypto_kem/round5-r5nd_5kem_0d/change.py
@@ -6,13 +6,14 @@ for dname, dirs, files in os.walk("clean"):
     # new_word_lst = list()
     # word_lst = list()
     func_prefix = 'PQCLEAN_ROUND5{{ scheme|upper }}_CLEAN_'
-    function_lst = ['ceil_log2', 'print_hex', 'print_sage_s_matrix', 'print_sage_s_vector', 'print_sage_s_vector_matrix', 'print_sage_u_matrix', 'print_sage_u_vector', 'print_sage_u_vector_matrix', 'checked_calloc', 'checked_malloc', 'checked_realloc', 'conditional_constant_time_memcpy', 'constant_time_memcmp']
+    #function_lst = ['ceil_log2', 'print_hex', 'print_sage_s_matrix', 'print_sage_s_vector', 'print_sage_s_vector_matrix', 'print_sage_u_matrix', 'print_sage_u_vector', 'print_sage_u_vector_matrix', 'checked_calloc', 'checked_malloc', 'checked_realloc', 'conditional_constant_time_memcpy', 'constant_time_memcmp']
+    function_lst = ['xef_reg']
     for fname in files:
         fpath = os.path.join(dname, fname)
         with open(fpath) as f:
             s = f.read()
         for function in function_lst:
-            s = s.replace(function + '(', func_prefix + function + '(')
+            s = s.replace(function, func_prefix + function)
             # print(func_prefix + function)
         # for word in s.split("("):
         #     if "PQCLEAN_ROUND5{{ scheme|upper }}" in word:
